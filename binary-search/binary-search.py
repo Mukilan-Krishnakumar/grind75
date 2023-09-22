25b9877e794e91d4c038c5b502c29e67bee75004
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-09-22 08:17:54 +0530

    Binary Search, took lot of time, stupid me

---
+++ binary-search/binary-search.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+
+s = Solution()
+print(s.search([1,2,4,6,8,9],6))
