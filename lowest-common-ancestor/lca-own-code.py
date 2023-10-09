5becfdf0c64fe16ad9dbdc2870caa627da4aa583
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-10-09 10:19:00 +0530

    Lowest Common Ancestor, with a little help from NeetCode

---
+++ lowest-common-ancestor/lca-own-code.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        current = root
+        while current.val != None:
+            if p.val < current.val and q.val < current.val:
+                current = current.left
+                self.lowestCommonAncestor(current, p, q)
+            elif p.val > current.val and q.val > current.val:
+                current = current.right
+                self.lowestCommonAncestor(current, p, q)
+            else:
+                return current
