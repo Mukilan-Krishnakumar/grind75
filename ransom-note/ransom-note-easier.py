95e4ed18bff720f407e6c797aacb433a94c089c3
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-10-11 19:29:03 +0530

    Convert it into list and check difference

---
+++ ransom-note/ransom-note-easier.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for char in ransomNote:
+            if char in magazine:
+                magazine.remove(char)
+            else:
+                return False
+        return True
